e62b26cd19c8835d3e04d1eabaf793c0480c72ec
Abdullah Sanver <a_sanver@hotmail.com>
2018-06-29 13:07:36 +0100

    Bismillahirrahmanirrahim. Sample project for Spring MVC.

---
+++ spittr/src/main/java/com/sanver/trials/spittr/home/HomeController.java
@@ -0,0 +1,14 @@
+package com.sanver.trials.spittr.home;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping(value = "/", method = GET)
+
+	public String home() {
+		return "home";
+	}
+}
